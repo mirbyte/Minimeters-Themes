1bb1a28796f0127a8b7b2cdf5e8ee2bdc7a121a9
Mirko <83219244+mirbyte@users.noreply.github.com>
2024-05-03 17:48:14 +0300

    Add files via upload

---
+++ minimeters theme installer.bat
@@ -0,0 +1,25 @@
+@echo off
+Title Minimeters Theme Installer [github mirbyte]
+echo.
+echo Is Minimeters data on C: drive? (Y/N)
+echo.
+echo.
+choice /c YN /n /m "Press Y for Yes, N for No:"
+if errorlevel 2 (
+	echo You chose "No". Running the script was aborted.
+	echo.
+	echo -----------------------------------------------
+	pause
+	exit
+) else (
+    for /r "%CD%" %%x in (*.txt) do copy "%%x" "C:\Users\%username%\AppData\Roaming\MiniMeters\themes"
+	if errorlevel 1 (
+		echo Error occurred while copying files.
+		echo.
+		echo -----------------------------------------------
+	) else (
+		echo.
+		echo -----------------------------------------------
+	)
+)
+pause
